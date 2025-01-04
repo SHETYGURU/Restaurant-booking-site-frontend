da0f10a0790db3bd14ee2338cfbcb9a48b233e46
SHETYGURU <shettygururaj279@gmail.com>
2025-01-04 22:33:34 +0530

    Your commit message

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'; 
 import { useRouter } from 'next/router'; 
-import './customStyles.css';
+// import './customStyles.css';
 import Calendar from '@/pages/Calendar';
 
 const Login = () => {
